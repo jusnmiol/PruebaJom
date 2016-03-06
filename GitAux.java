fbcbe9c467ca49a8f2b96d2c158cf60c9e254fcd
jmo <jmortiz@lcc.uma.es>
2016-03-06 10:20:31 +0100

    testing en GitAux

--- GitAux.java
+++ GitAux.java
@@ -21,4 +21,9 @@ public class GitAux {
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
     }
+
+    public void testGitAux(){
+	//método rama testing
+        System.out.println("Método de testing");
+    }
 }
